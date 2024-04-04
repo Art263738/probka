646f066095afc530de57768d24c1bbb9e9df5112
Соня <151829772+Art263738@users.noreply.github.com>
2024-04-04 16:14:58 +0900

    add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <nav>
     <div class="nav-container">
-    <a href="/" class="nav-logo" title="Back to Homepage">Лера Пименова </a>
+    <a href="/" class="nav-logo" title="Back to Homepage">Артюкова Софья </a>
      <div class="nav-links">
         {#each nav as link}
 
